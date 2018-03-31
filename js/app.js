d234429784e3b40d0f01a8207a3e86856ce4b1c4
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-03-31 10:28:37 +0100

    Remove unused modal content

--- js/app.js
+++ js/app.js
@@ -203,26 +203,14 @@ function startTimer() {
     }
 }
 
-/** Adapted from https://www.w3schools.com/howto/howto_css_modals.asp **/
-// Get the modal
+/** Modal JS **/
 var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
-}
+var span = document.getElementById("close");
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
