b151210206e930c48b479a58b4d98cfa03368ee1
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-03-31 14:05:33 +0100

    Update moveCounter function and comments

--- js/app.js
+++ js/app.js
@@ -63,17 +63,6 @@ function shuffle(array) {
     return array;
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
 // set up the event listener for a card. If a card is clicked:
 deck.addEventListener('click', function (e) {
     // script will run if element clicked is a valid card
@@ -158,11 +147,8 @@ function updateMoveCounter() {
     const moveCounter = document.getElementById('moves');
     player.makeMove();
     const moves = player.moves;
-    if (moves === 1) {
-        moveCounter.textContent = moves + ' move';
-    } else {
-        moveCounter.textContent = moves + ' moves';
-    }
+    // render the move count
+    moveCounter.textContent = moves === 1 ? (moves + ' move') : (moves + ' moves');
 }
 
 function checkAllCardsMatch() {
